76ee578eb73bf9ed81edb326ece173641a7354f7
mgbGigliotti <mgbgigliotti@gmail.com>
2026-02-19 16:56:13 -0300

    Delete Aula02Exerc02.py

--- Aula02Exerc02.py
+++
@@ -1,4 +0,0 @@
-Dias = int(input("Digite a quantidade de Dias: "))
-KM = int(input("Digite a quantidade de quilometros percorridos: "))
-Preço = KM * 0.15 + Dias * 60.00
-print("Seu preço a pagar é: ",Preço)
